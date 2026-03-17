9d6b2b5bdb9f97962aad77e42378406911a6e991
Chandresh Sharma <100024998+Chandresh-Sharma-1007@users.noreply.github.com>
2026-03-17 15:11:35 +0530

    Update HoliGeet.js

--- HoliGeet.js
+++ HoliGeet.js
@@ -5,7 +5,7 @@
 // NOTICE: We do NOT use 'const'. We use 'window.appData["Aarti"]'
 // This means: "Find the existing appData and put this inside it."
 
-window.appData["Holi geet"] = {
+window.appData["Holi Geet] = {
     icon: " <img src='holi_geet.png ' style='width: 180px; '>",
     items: [
     
