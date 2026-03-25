82aca65a6a8695a1e89f8815104e546b589f6e54
Chandresh Sharma <100024998+Chandresh-Sharma-1007@users.noreply.github.com>
2026-03-25 11:43:52 +0530

    Add files via upload

---
+++ Nathji_Bhajan.js
@@ -0,0 +1,417 @@
+// STEP 2: The Specific Songs for THIS file
+window.appData["Bhajans"].subcategories[" Nathji Bhajan "] = [
+
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ramdev_Bhajan.js
@@ -0,0 +1,417 @@
+// STEP 2: The Specific Songs for THIS file
+window.appData["Bhajans"].subcategories[" Ramdev Bhajan "] = [
+
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+// {
+//     title: "",
+//     youtubeId: "", // I added a working ID here for you
+//     lyrics: `.`
+// },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
